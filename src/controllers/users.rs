d71fc89d54696fb8905c852dff9cffcb4f3b0932
Adrien Chapelet <adrien.chapelet@iothings.fr>
2023-08-01 07:51:27 +0200

    feat: wip on user update from id

--- src/controllers/users.rs
+++ src/controllers/users.rs
@@ -5,7 +5,9 @@ use crate::{
 };
 use actix_web::{delete, get, post, put, web, HttpResponse};
 use json;
-use mongodb::{bson, bson::Document, bson::doc, Client, Collection};
+use mongodb::{bson::{doc, self}, Client, Collection};
+
+use super::error;
 
 /// Adds a new user to the "users" collection in the database.
 #[post("/")]
@@ -63,9 +65,12 @@ pub async fn get_user_by_email(
 pub async fn update_user(
     auth: Authenticated,
     client: web::Data<Client>,
+    id: web::Path<String>,
     body: web::Bytes,
 ) -> HttpResponse {
     log::debug!("auth: {auth:?}");
+    let user_id = id.into_inner();
+
     let json_parse_res = json::parse(std::str::from_utf8(&body).unwrap()); // return Result
     let user_in_json: json::JsonValue = match json_parse_res {
         Ok(v) => v,
@@ -74,15 +79,32 @@ pub async fn update_user(
 
     match User::from_json_value(&user_in_json) {
         Some(new_user) => {
+            let collection: Collection<User> = client.database(DB_NAME).collection(users::REPOSITORY_NAME);
+
+            let old_user: User;
+            //mongodb::bson::oid::ObjectId::from_str("id_str").unwrap();
+            match collection.find_one(doc! { "_id": user_id }, None).await {
+                Ok(Some(user)) => old_user = user,
+                Ok(None) => old_user = new_user.clone(),
+                Err(err) => {
+                    log::error!("No user found with email while updating: {err}");
+                    old_user = new_user.clone()
+                },
+            }
+
+            let filter = doc! {"email": &old_user.clone().email};
+            /*let new_user_bson = match bson::to_bson(&new_user.clone()) {
+                Ok(bson_obj) => bson_obj,
+                Err(err) => {
+                    log::error!("User to bson err: {err}");
+                    bson::to_bson(&new_user.clone()).unwrap()
+                },
+            };*/
             let new_user_copy = new_user.clone();
-            let collection: Collection<User> =
-                client.database(DB_NAME).collection(users::REPOSITORY_NAME);
-            let filter = doc! {"email": new_user_copy.email};
-            let new_user_bson = match bson::to_bson(&new_user_copy) {
-                Document(doc) => doc,
-                _ => unreachable!(),
-            };
+            let new_user_bson = bson::to_bson(&new_user_copy).unwrap();
+            //let user_doc = new_user_bson.as_document().unwrap();
             let update = doc! {"$set": new_user_bson };
+            //let update = doc! {"$set": {"first_name": new_user_copy.first_name}};
             let result = collection.update_one(filter, update, None).await;
             match result {
                 Ok(_) => HttpResponse::Ok().json(new_user),
@@ -93,7 +115,7 @@ pub async fn update_user(
                 }
             }
         }
-        None => HttpResponse::InternalServerError().body("Parsing error"),
+        None => HttpResponse::InternalServerError().body("User from json error"),
     }
 }
 
