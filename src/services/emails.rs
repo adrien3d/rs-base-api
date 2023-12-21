760b491ce5bc8539b5fac2d4408cff8b43856cf6
Adrien Chapelet <adrien3d@gmail.com>
2023-12-21 07:22:05 +0100

    fix: aws new sdk

--- src/services/emails.rs
+++ src/services/emails.rs
@@ -1,33 +1,41 @@
 use aws_sdk_sesv2::types::{Body, Content, Destination, EmailContent, Message};
 
 pub async fn send_email_with_aws_ses(
-    _dest: &str,
-    _subject: &str,
-    _message: &str,
+    dest: &str,
+    subject: &str,
+    message: &str,
 ) -> anyhow::Result<()> {
-    // let config = aws_config::load_from_env().await;
-    // let client = aws_sdk_sesv2::Client::new(&config);
+    let config = aws_config::load_from_env().await;
+    let client = aws_sdk_sesv2::Client::new(&config);
 
-    // let mut destination: Destination = Destination::builder().build();
-    // destination.to_addresses = Some(vec![dest.to_string()]);
-    // let subject_content = Content::builder().data(subject).charset("UTF-8").build();
-    // let body_content = Content::builder().data(message).charset("UTF-8").build();
-    // let body = Body::builder().text(body_content).build();
+    let mut destination: Destination = Destination::builder().build();
+    destination.to_addresses = Some(vec![dest.to_string()]);
+    let subject_content = Content::builder()
+        .data(subject)
+        .charset("UTF-8")
+        .build()
+        .expect("Building subject content");
+    let body_content = Content::builder()
+        .data(message)
+        .charset("UTF-8")
+        .build()
+        .expect("Building body content");
+    let body = Body::builder().text(body_content).build();
 
-    // let msg = Message::builder()
-    //     .subject(subject_content)
-    //     .body(body)
-    //     .build();
+    let msg = Message::builder()
+        .subject(subject_content)
+        .body(body)
+        .build();
 
-    // let email_content = EmailContent::builder().simple(msg).build();
+    let email_content = EmailContent::builder().simple(msg).build();
 
-    // client
-    //     .send_email()
-    //     .from_email_address("no-reply@iothings.fr")
-    //     .destination(destination)
-    //     .content(email_content)
-    //     .send()
-    //     .await?;
+    client
+        .send_email()
+        .from_email_address("no-reply@iothings.fr")
+        .destination(destination)
+        .content(email_content)
+        .send()
+        .await?;
 
     Ok(())
 }
